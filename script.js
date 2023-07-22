c9873f2be2a6b424303902ce4c79f42aea31dfbf
Khan Imran <98589155+erkhanimran@users.noreply.github.com>
2023-07-23 02:48:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function getFormvalue() {
-    //Write your code here
+const form = document.getElementById("form1");
 
+function getFormvalue(){
+    alert(form["fname"].value +" "+form["lname"].value);
 }
